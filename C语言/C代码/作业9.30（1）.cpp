3956bb8786ca34af2e6c94ee3bda5e22f8eef661
ReverseRobot <reverse.wky@outlook.com>
2024-12-06 22:43:56 +0800

    Delete C语言 directory

--- C语言/C代码/作业9.30（1）.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-//By ChenAo
-int main() {
-	printf("Hello World!\n");//1.4
-	printf("*****\n *****\n  *****\n   *****");//1.5
-	return 0;
-}
--- C语言/C代码/输入数字输出月份.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	int i;
-	char month[12][5] = {"ja", "fe", "qw"};
-	char (*p)[5] = month;
-	scanf("%d", &i);
-	printf("%s", *(p + i - 1));
-}
